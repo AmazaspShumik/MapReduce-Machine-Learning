c2d5b5554da0f7236ffa4eaed3fbfa268131c2c4
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-06-05 17:53:43 -0700

    added scaling

--- RidgeRegression/RidgeRegressionMapReduce.py
+++ RidgeRegression/RidgeRegressionMapReduce.py
@@ -10,6 +10,32 @@ import numpy as np
 import random
 
 
+# ----------------------------- Helper Classes & Methods --------------------------------
+
+def cholesky_solution_least_squares(part_one, part_two):
+    '''Cholesky decomposition '''
+    R     = np.linalg.cholesky(part_one)
+    z     = np.linalg.solve(R,part_two)
+    theta = np.linalg.solve(np.transpose(R),z)
+    return theta
+
+
+
+class RidgeRegressionHoldOutCV(object):
+    
+    def __init__(self,lambdas):
+        self.lambdas = lambdas
+        self.data = []
+
+    
+    def add_mapper_output(self,data):
+        pass
+    
+    
+    def cv(self):
+        pass
+
+
 
 class RidgeRegression(MRJob):
     '''
@@ -29,10 +55,7 @@ class RidgeRegression(MRJob):
           -- dimension  - (int)  number of explanatory variables
           -- bias       - (bool) if True regression wil include bias term
     
-    
-    
-    
-    
+
     
     '''
     
@@ -50,6 +73,7 @@ class RidgeRegression(MRJob):
         self.mu = [0]*self.dim
         self.y_av = 0.0
         self.x_t_x = np.zeros([self.dim,self.dim], dtype = np.float)
+        self.x_t_y = [0]*self.dim
         self.n = 0
         self.lambdas_cv = self.read_lambdas(self.options.cv_lambdas)
         self.sample_cv = []
@@ -84,6 +108,10 @@ class RidgeRegression(MRJob):
             self.option_parser.error("You need to specify name of file with set of regularisation parameters")
         # sample size for hold out cross validation
         self.cv_size = self.options.hold_out_sample_size
+        # scaling options
+        if self.options.scaling not in [None,'z-score','max-min']:
+            self.options_parser.error("You need to define proper scaling ('z-score' or 'max-min')")
+            
         
     #----------------------------------------- helper functions ----- --------------------------------------------#
         
@@ -97,6 +125,7 @@ class RidgeRegression(MRJob):
         y = float(data[-1])
         return (y,features)
       
+      
     @staticmethod
     def read_lambdas(filename):
         ''' reads regularisation parameters'''
@@ -104,8 +133,45 @@ class RidgeRegression(MRJob):
             lambdas = list(csv.reader(csvfile))
         return [float(e) for e in lambdas]
         
-    @staticmethod
-    def join_mapper_intermediate_stats( mapper_dict_one, mapper_dict_two):
+        
+    def join_mapper_intermediate_stats(self, mapper_one, mapper_two):
+        '''
+        Aggregates mapper outputs
+        '''
+        mapper_one["mu"]    = [mapper_one["mu"][i] + mapper_two[i] for i in range(self.dim)]
+        sum_lists = lambda x,y,n: [x[i] + y[i] for i in range(n)]
+        xtx_1, xtx_2 = mapper_one["x_t_x"], mapper_two["x_t_x"] 
+        mapper_one["x_t_x"] = [sum_lists(xtx_1[i],xtx_2[i],self.dim) for i in range(self.dim)]
+        mapper_one["y_av"] += mapper_two["y_av"]
+        mapper_one["n"]    += mapper_two["n"]
+        if self.options.scaling == "max-min":
+            mapper_one["max"] = [max(mapper_one["max"][i],mapper_two["max"][i]) for i in range(self.dim)]
+            mapper_one["min"] = [min(mapper_one["min"][i],mapper_two["min"][i]) for i in range(self.dim)]
+        return mapper_one
+        
+    
+    def estimate_params(self,data,lambda_ridge,scaling = None):
+        xtx   = np.array(data["x_t_x"])
+        xty   = np.array(data["x_t_y"]) 
+        mu    = np.array(data["mu"])
+        y_av  = data["y_av"]
+        n     = data["n"]
+        beta_bias   = y_av # (bias terms)
+        if scaling is None:
+           part_one  = xtx - n*np.outer(mu,mu)+lambda_ridge*np.eye(self.dim)
+           part_two  = xty - n*y_av*mu
+        elif scaling == "z_score":
+           sigma     = 1.0/np.sqrt(np.diag((1.0/n*(xtx-np.outer(mu,mu))))) # vector of standard deviations
+           scaler    = np.outer(sigma,sigma)
+           part_one  = np.dot(scaler,xtx-n*np.outer(mu,mu)) + lambda_ridge*np.eye(self.dim)
+           part_two  = sigma*xty - sigma*mu*y_av*n
+        elif scaling == "max-min":
+           scale_vec = 1.0/( np.array(data["max"]) - np.array(data["min"]) )
+           scaler    = np.outer(scale_vec,scale_vec)
+           part_one  = np.dot(scaler,xtx-n*np.outer(mu,mu)) + lambda_ridge*np.eye(self.dim)
+           part_two  = scale_vec*xty - scale_vec*mu*y_av*n
+        theta = cholesky_solution_least_squares(part_one, part_two)
+        return {"bias_term": beta_bias,"theta":list(theta)}
         
         
         
@@ -119,6 +185,7 @@ class RidgeRegression(MRJob):
             self.max = [max(current_max,features[i]) for i,current_max in enumerate(features)]
             self.min = [max(current_max,features[i]) for i,current_max in enumerate(features)]
         self.mu    = [ av+features[i] for i,av in enumerate(self.mu) ]
+        self.x_t_y = [ xty_i + y*features[i] for xty_i,i in enumerate(features)]
         self.x_t_x = np.outer(x,x)
         self.y_av +=y
         self.n    +=1
@@ -133,39 +200,50 @@ class RidgeRegression(MRJob):
             if observation[0] > self.sample_cv[0][0]:
                 heapq.heapreplace(self.sample_cv,observation)
                 
+                
     def mapper_ridge_final(self):
-        make_json_encodable = lambda x: [list(row) for row in x]
-        x_t_x = self.make_json_encodable(self.x_t_x)
-        yield None, {"mu":    self.mu,
-                     "x_t_x": x_t_x,
-                     "y_av":  self.y_av,
-                     "n":     self.n}
-                     
-    def reducer_ridge(self, key, vals):
-        # first calculates unscaled solution
-        final_summary_stats = {"mu":      [0]*self.dim,
-                      "x_t_x":   np.zeros([self.dim,self.dim]),
-                      "y_av":    0,
-                      "n":       0  }
-        for mapper_summary in vals:
-            final_summary_stats = self.join
+        x_t_x = [list(row) for row in self.x_t_x] # transform numpy array to json-encodable data structure
+        intermediate_stats = {"mu":    self.mu,
+                              "x_"
+                              "x_t_x": x_t_x,
+                              "y_av":  self.y_av,
+                              "n":     self.n}
+        if self.options.scaling == "max-min":
+            intermediate_stats["max"] = self.max
+            intermediate_stats["min"] = self.min
         
-        
-        
+        yield None, ("stats",intermediate_stats)
+        yield None, ("hold_out_cv",self.sample_cv)
             
-        
-        
-        
-        
-        
-        
-        
-        
+                  
+                  
+    def reducer_ridge(self, key, vals):
+        ridge = RidgeRegressionHoldOutCV(self.lambdas)
+        final_summary_stats = {"mu":      [0]*self.dim,
+                               "x_t_x":   [[0]*self.dim for i in range(self.dim)],
+                               "x_t_y":   [0]*self.dim,
+                               "y_av":    0,
+                               "n":       0  }
+        for val in vals:
+            if val[0]=="stats":
+                mapper_summary = val[1]
+                final_summary_stats = self.join_mapper_intermediate_stats(final_summary_stats,mapper_summary)
+            else:
+                ridge.add_mapper_output(val[1])
+        # for each scaling type use cross validation to verify best lambda
+        # then use it on all data (including cv set) to find parameters
+        best_lambda = ridge.cv(self.options.scaling)
+        yield None, self.estimate_params(final_summary_stats,best_lambda,self.options.scaling)
+
+            
+            
     def steps(self):
         return [MRStep(mapper       = self.mapper_ridge,
-                       mapper_final = self.mapper_ridge_final)]
+                       mapper_final = self.mapper_ridge_final,
+                       reducer      = self.reducer_ridge)]
                        
 if __name__=="__main__":
+    RidgeRegression.run()
     
         
         
